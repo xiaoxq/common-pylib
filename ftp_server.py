7cc92a430c7e3d5b7cb0240827aa76a34a9c6161
Aaron Xiao <xiaoxiangquan@baidu.com>
2017-01-16 10:02:02 -0800

    ADD: Ftp server.

---
+++ ftp_server.py
@@ -0,0 +1,27 @@
+"""FTP Server."""
+#!/usr/bin/env python
+import pyftpdlib.authorizers
+import pyftpdlib.handlers
+import pyftpdlib.servers
+import sys
+
+import config
+
+
+def serve():
+    """Server entry."""
+    authorizer = pyftpdlib.authorizers.DummyAuthorizer()
+    authorizer.add_user(config.get('ftp_user'),
+                        config.get('ftp_pass'),
+                        config.get('ftp_root'))
+
+    handler = pyftpdlib.handlers.FTPHandler
+    handler.authorizer = authorizer
+
+    server = pyftpdlib.servers.FTPServer((config.get('ftp_host'), int(config.get('ftp_port'))),
+                                         handler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    config.init(sys.argv[1], 'ftp_server')
+    serve()
